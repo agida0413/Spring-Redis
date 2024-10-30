029dd849bd1f6d88f3f61c92557f5a9604da434b
KimYJInComapny <yongjjun8718@gmail.com>
2024-10-30 17:00:34 +0900

    first commit

---
+++ src/test/java/yj/redis_spring/RedisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package yj.redis_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
